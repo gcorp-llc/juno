e4ef501ea0505fcd8355a6da22ece47df72573d4
GCORP LLC <hosseinradvictor@gmail.com>
2024-08-01 23:18:47 +0330

    Install Filament Translate

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -8,6 +8,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
+use Filament\SpatieLaravelTranslatablePlugin;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -42,7 +43,8 @@ class AdminPanelProvider extends PanelProvider
                     ->navigationSort(2)
                     ->navigationCountBadge()
                     ->defaultListView('grid' || 'list'),
-
+                SpatieLaravelTranslatablePlugin::make()
+                    ->defaultLocales(['fa', 'ar',"en"]),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
