36241d0e3d1c750dc1a5ab9411bad0402c987ec5
GCORP LLC <hosseinradvictor@gmail.com>
2024-08-01 22:27:46 +0330

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
-  content: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
   theme: {
     extend: {},
   },
-  plugins: [],
+    plugins: [
+        daisyui,
+    ],
 }
 
