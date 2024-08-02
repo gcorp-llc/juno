420ddca36058fab46a3ce13c8a919093f3e3f61e
GCORP LLC <hosseinradvictor@gmail.com>
2024-08-02 10:15:31 +0330

    set livewire config And Create Pages

---
+++ app/Livewire/Pages/Tune.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class Tune extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.tune');
+    }
+}
--- routes/route.php
+++ routes/route.php
@@ -1,13 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
+
 Route::get('/', \App\Livewire\Pages\Home::class)->name('home');
-Route::get('/articles', \App\Livewire\Pages\Posts::class)->name('posts');
+Route::get('/posts', \App\Livewire\Pages\Posts::class)->name('posts');
+Route::get('/gallery', \App\Livewire\Pages\Gallery::class)->name('gallery');
 Route::get('/faq', \App\Livewire\Pages\Faq::class)->name('faq');
 Route::get('/contact_us', \App\Livewire\Pages\Contact::class)->name('contact_us');
 Route::get('/about_us', \App\Livewire\Pages\About::class)->name('about_us');
+Route::get('/tune', \App\Livewire\Pages\Tune::class)->name('tune');
 
-Route::get('dr/{slug}',\App\Livewire\Show\Doctor::class)->name('dr.show');
-Route::get('article/{slug}',\App\Livewire\Show\Post::class)->name('article.show');
 
 
